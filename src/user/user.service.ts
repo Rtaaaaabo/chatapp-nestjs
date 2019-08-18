ce11727c0c1cc8fdb43160e7786c1ef66056d571
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-08-18 19:12:53 +0900

    User Serviceの作成

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { User } from '../models/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService { }
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) { }
+
+    async findByEmail(email: string): Promise<User> {
+        return await this.userRepository.findOne({
+            where: {
+                email: email,
+            },
+        });
+    }
+    async create(user: User): Promise<User> {
+        return await this.userRepository.save(user);
+    }
+}
