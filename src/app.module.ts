4216dfd084a8bf0923dadce652d8d69b0042eb81
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-08-18 17:50:46 +0900

    Entityの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './models/user.entity';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'my.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
